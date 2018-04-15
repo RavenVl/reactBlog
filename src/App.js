3a45691e3881b3ac1fdbc4fc888ba2130244483c
Raven <ravenvl71@gmail.com>
2018-04-15 17:22:49 +0300

    1

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+// import logo from './logo.svg';
+import Blog from './components/Blog';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <div className="demo-ribbon">
+                    <div className="demo-header">
+                        <div>
+                            <h3>Country News</h3>
+                            <div>Top stories of my country</div>
+                        </div>
+                        <div className="left-header">
+                            <div className="icon-container">
+                                <i className="fas fa-user"></i>
+                                <i className="fab fa-facebook"></i>
+                                <i className="fab fa-instagram" aria-hidden="true"></i>
+                                <i className="fab fa-vk"></i>
+                            </div>
+                            <div className="header-link">
+                                <div>Link1</div>
+                                <div>Link2</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <Blog />
+            </div>
+
+        );
+    }
+}
+
+export default App;
+
---
+++ src/components/Blog.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+class Blog extends React.Component{
+    render(){
+        return (
+            <div className="demo-content">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid asperiores, corporis culpa cumque
+                delectus dolor enim eum hic maiores nam necessitatibus, officiis optio quae quis repudiandae
+                temporibus ut, velit vitae.
+            </div>
+        )
+    }
+}
+export default Blog;
